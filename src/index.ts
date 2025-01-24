ab89054720414084834c90a0e557d94cecdf1edb
Harshal Katakiya <katakiyaharshl001@gmail.com>
2025-01-24 15:49:53 +0530

    project setup for new code

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export { getAxiosInstance } from './axiosInstance.js';
-export { getAxiosConfig, setAxiosConfig } from './config/axiosConfig.js';
-export { makeRequest } from './makeRequest.js';
-export * from './types/types.js';
-export { useAxios } from './useAxios.js';
-export * from './utils/utils.js';
